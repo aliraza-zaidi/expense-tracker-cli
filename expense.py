a52e24217af9aaa78eece6e5067ca87a6dd91625
aliraza-zaidi <alirazaidi.official@gmail.com>
2025-06-26 23:50:07 +0500

    Made Expense class serializable

--- expense.py
+++ expense.py
@@ -11,3 +11,18 @@ class Expense:
 
     def __str__ (self):
         return f"Expense ID: {self.id}\nTitle: {self.title}\nAmount: {self.amount}\nCategory: {self.category}\nDate: {self.date}\nNotes: {self.notes}"
+
+    def serialize (self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "amount": self.amount,
+            "category": self.category,
+            "date": self.date.strftime("%Y-%m-%d"),
+            "notes": self.notes,        
+        }
+    
+    @staticmethod
+    def deserialize (data):
+        return Expense(data["id"], data["title"], data["amount"],
+             data["category"], data["date"], data["notes"])
