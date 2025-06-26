595f9b13a04af329309ba1f4e318c6ffdf86823f
aliraza-zaidi <alirazaidi.official@gmail.com>
2025-06-26 23:54:53 +0500

    Implemented add_expense()

--- expense_tracker.py
+++ expense_tracker.py
@@ -10,3 +10,15 @@ class ExpenseTracker:
         self.expenses = dict()
         self.categories = set()
         self.load_data()
+
+    def add_expense (self, title, amount, category, date, notes=None):
+        try:
+            expense = Expense(self.ID, title, amount, category, date, notes)
+            self.expenses[self.ID] = expense
+            self.ID += 1
+            if category not in self.categories:
+                self.categories.add(category)
+            print("Expense Added Successfully!")
+            self.save_data()     
+        except:           
+            print("Error in adding expense") 
